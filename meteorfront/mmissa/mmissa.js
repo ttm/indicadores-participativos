c3ff97cf9f3133388b2d5190996e328865c8d613
Renato Fabbri <renato.fabbri@gmail.com>
2014-05-16 15:00:35 -0300

    feita primeira versao da mmissa. acionando primeiro controle na propria interface

--- meteorfront/mmissa/mmissa.js
+++ meteorfront/mmissa/mmissa.js
@@ -3,17 +3,37 @@ Meteor.setInterval(function () {
   Session.set('time', new Date);
 }, 1000);
 
-
-  Template.contador.segundos= function () {
+  Template.mmissa.texto= function(){
+    return "MMISSA";
+}
+  Template.contador.mousepos= function () {
+    //return [10,20];
+    return [Session.get("mx"),Session.get("my")];
+};
+  Template.contador.contagem= function () {
     ttime=Session.get("time")
+    d3.select("#textoMmissa").transition().attr("x",(84+2*Math.random())+"%")
+                        .attr("y",(44+2*Math.random())+"%")
+                        .attr("font-size",15+10*Math.random());
     return [ttime.getMinutes(),ttime.getSeconds()];
   };
+
+  Template.mmissa.events({
+    'click': function () {
+        d3.select("#rectMmissa").transition().style("fill","rgb("+256*Math.random()+","+256*Math.random()+","+256*Math.random()+")").attr("height",(10+20*Math.random())+"%");
+    },
+  });
+
 
   Template.hello.events({
     'click': function () {
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
-    }
+    },
+     'mousemove': function(evt){
+        Session.set("mx",evt.clientX);
+        Session.set("my",evt.clientY);
+}
   });
 }
 
