d7a3d08ed4390763edffb881bb419f4c5df72019
Renato Fabbri <renato.fabbri@gmail.com>
2014-05-16 17:26:21 -0300

    colocado menu

--- meteorfront/mmissa/mmissa.js
+++ meteorfront/mmissa/mmissa.js
@@ -11,46 +11,66 @@ Meteor.setInterval(function () {
     return [Session.get("mx"),Session.get("my")];
 };
   Template.contador.contagem= function () {
-    ttime=Session.get("time")
     d3.select("#textoMmissa").transition().attr("x",(84+2*Math.random())+"%")
                         .attr("y",(44+2*Math.random())+"%")
                         .attr("font-size",15+10*Math.random());
-    return [ttime.getMinutes(),ttime.getSeconds()];
+
+    ttime=Session.get("time")
+    if(ttime)
+        return [ttime.getMinutes(),ttime.getSeconds()];
   };
 
   Template.mmissa.events({
     'click': function () {
-        d3.select("#rectMmissa").transition().attr("height",(20+20*Math.random())+"%");
+        ht=Math.floor(20+20*Math.random())+"%";
+    d3.select("#rectMmissa").transition().attr("height",ht);
     d3.selectAll("rect").style("fill",function(d){
         acor="rgb("+Math.floor(256*Math.random())+","+Math.floor(256*Math.random())+","+Math.floor(256*Math.random())+")";
         return acor;});
-    }
+    },
   });
 
   Template.controladores.events({
     'click': function(evt){
-    ee=evt;
-    px=evt.offsetX;
-    py=evt.offsetY;
-    ee0=d3.select("#rectControladores");
-    fx=ee0.property("x").baseVal.value;
-    fy=ee0.property("y").baseVal.value;
-    rx=px-fx;
-    ry=py-fy;
-    d3.selectAll("rect").attr("rx",rx).attr("ry",ry);
+        var px=evt.offsetX;
+        var py=evt.offsetY;
+        var ee0=d3.select("#rectControladores");
+        var fx=ee0.property("x").baseVal.value;
+        var fy=ee0.property("y").baseVal.value;
+        var rx=px-fx;
+        var ry=py-fy;
+        d3.selectAll("rect").attr("rx",rx).attr("ry",ry);
 },
 });
 
   Template.hello.events({
-    'click': function () {
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    },
      'mousemove': function(evt){
         Session.set("mx",evt.clientX);
         Session.set("my",evt.clientY);
 }
   });
+  Template.hello.rendered=function(){
+    topicos=["aa","hashtags","participabr","arenanetmundial","teloes","escritos","emails","doação de dados","sobre"];
+    ee0=d3.select("#rectC");
+    fx=ee0.property("x").baseVal.valueInSpecifiedUnits;
+    fy=ee0.property("y").baseVal.valueInSpecifiedUnits;
+    width=ee0.property("width").baseVal.valueInSpecifiedUnits;
+    height=ee0.property("height").baseVal.valueInSpecifiedUnits;
+    menu=[];
+    for(var i=0; i<topicos.length; i++){
+        var x=(0.2+i%3)*(width/3);
+        var y=(1+Math.floor(i/3))*(height/4);
+        menu.push({palavra:topicos[i],x:fx+x,y:fy+y});
+}
+    ee1=d3.select("#svg0");
+    ee1.selectAll("text #menuText").data(menu)
+                .enter().append("text")
+                .text(function (d){return d.palavra})
+                        .attr("font-size",15+10*Math.random())
+                .attr("x", function(d) { return d.x+"%"; })
+               .attr("y", function(d)  { return d.y+"%";});
+};
+
 }
 
 if (Meteor.isServer) {
