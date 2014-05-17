a58f2c1545e300eb90fec27a4028d0fc83e96143
Renato Fabbri <renato.fabbri@gmail.com>
2014-05-17 13:14:40 -0300

    feita toda base para colocar os hashtags ou qqr opcao

--- meteorfront/mmissa/mmissa.js
+++ meteorfront/mmissa/mmissa.js
@@ -12,9 +12,14 @@ Meteor.setInterval(function () {
     d3.select("#textoMmissa").transition().attr("x",(84+2*Math.random())+"%") //TTM
                         .attr("y",(44+2*Math.random())+"%")
                         .attr("font-size",15+10*Math.random());
-    ttime=Session.get("time")
-    if(ttime)
+    ttime=Session.get("time");
+    d3.selectAll(".sobreText").style("fill",function(){
+        acor="rgb("+Math.floor(256*Math.random())+","+Math.floor(256*Math.random())+","+Math.floor(256*Math.random())+")";
+        return acor;
+    });
+    if(ttime){
         return [ttime.getMinutes(),ttime.getSeconds()];
+    }
   };
 
   Template.mmissa.events({
@@ -47,41 +52,81 @@ Meteor.setInterval(function () {
   });
   Template.hello.rendered=function(){
     topicos=["aa","hashtags","participabr","arenanetmundial","teloes","escritos","emails","doação de dados","sobre"];
-    //ee0=d3.select("#rectC");
-    //fx=ee0.property("x").baseVal.valueInSpecifiedUnits;
-    //fy=ee0.property("y").baseVal.valueInSpecifiedUnits;
-    //width=ee0.property("width").baseVal.valueInSpecifiedUnits;
-    //height=ee0.property("height").baseVal.valueInSpecifiedUnits;
     menu=[];
     for(var i=0; i<topicos.length; i++){
         var x=(0.2+i%3)*(100.0/3);
-        var y=(1+Math.floor(i/3))*(100.0/4);
+        var y=(0.6+Math.floor(i/3))*(100.0/5);
         menu.push({palavra:topicos[i],x:x,y:y});
 }
     ee1=d3.select("#svgC");
-    groups=ee1.selectAll("g #menuItem").data(menu)
-      .enter().append("svg").attr("id",function(d){return "id"+d.palavra;})
+    groups=ee1.selectAll("g").data(menu)
+      .enter().append("svg").attr("class","menuItem").attr("id",function(d){return "id"+d.palavra;})
+                .style("fill", "white")
                 .attr("x", function(d) { return d.x-1+"%"; })
                .attr("y", function(d)  { return d.y-4+"%";});
                groups.append("rect")
-                .style("fill", "white")
+                .attr("y", function(d)  { return "0%";})
                 .attr("width", function(d) { return "25%"; })
-                .attr("height", function(d) { return "5%"; });
-
+                .attr("height", function(d) { return "12%"; });
     groups.append("text")
-                .attr("x", function(d) { return "1%"; })
-               .attr("y", function(d)  { return "4%";})
+                .style("fill", "black")
+                .attr("x", function(d) { return "1%"; }).style("stroke-width","0%")
+               .attr("y", function(d)  { return "6%";})
                 .text(function (d){return d.palavra});
+    SELECTED=0;
     groups.on("click",function(d){
-                        ddd=d;
-                        aa=d3.select(this).attr("x","22%");
-});
-// quero que quando eu clique, sobre só o que eu clique,
-// lá encima. O resto some.
-
+                    if(SELECTED){
+                        SELECTED=0;
+                            d3.selectAll(".menuItem").transition().style("fill","white").style("stroke-width","0%")
+                .attr("x", function(d) { return d.x-1+"%"; })
+               .attr("y", function(d)  { return d.y-4+"%";});
+                    } else {
+                                d3.selectAll(".menuItem").transition().attr("x","60%").attr("y","5%").style("fill","black");
+                                d3.select(this).transition().attr("x","5%").attr("y","5%").style("fill","yellow").style("stroke-width","0.3%").style("stroke","black");
+                                SELECTED=this.id;
+                        }
+    Session.set("SELECTED",SELECTED);
+      });
+};
+Template.tCentral.SELECTED=function(){
+    Session.get("SELECTED");
+    if(typeof SELECTED=="undefined"){
+        return 0;
+    } else {
+        return SELECTED;
+    }
+};
+Template.tCentral.isHash=function(){
+    Session.get("SELECTED");
+    if(typeof SELECTED=="undefined"){
+        return 0;
+    } else {
+        if(SELECTED=="idhashtags"){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+};
+Template.tCentral.isSobre=function(){
+    Session.get("SELECTED");
+    if(typeof SELECTED=="undefined"){
+        return 0;
+    } else {
+        if(SELECTED=="idsobre"){
+            return 1;
+        } else {
+            if(SELECTED=="idhashtags" || SELECTED==0){
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
 };
 
-                    }
+ 
+                    } // end isClient
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
